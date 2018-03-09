e27fa66322d42c0cc9a22af7d55fffe4546b11a3
Jim Bo <jim@rath.us>
2018-03-09 16:50:04 -0600

    adding translation button to client Voting.Document

--- client/src/components/voting/Document.js
+++ client/src/components/voting/Document.js
@@ -1,13 +1,55 @@
 import React, { Component } from 'react';
+import Mousetrap from 'mousetrap';
 import './Document.css';
 
+const translationServiceUrls = {
+  bing: 'https://www.bing.com/translator/?to=en&text=',
+  google: 'https://translate.google.com/#auto/en/',
+  yandex: 'https://translate.yandex.com/?lang=en&text=',
+};
+
 class Document extends Component {
+  constructor(props) {
+    super(props);
+    this.handleTranslationClick = this.handleTranslationClick.bind(this);
+  }
+
+  handleTranslationClick() {
+    const query = encodeURIComponent(this.formatDocument());
+    const baseUrl = translationServiceUrls[process.env.REACT_APP_TRANSLATION_SERVICE];
+    const url = baseUrl + query;
+    if (this.translationWindow && !this.translationWindow.closed) {
+      this.translationWindow.location = url;
+      this.translationWindow.focus()
+    }
+    else {
+      this.translationWindow = window.open(url, '_blank');
+    }
+  }
+
+  componentDidMount() {
+    Mousetrap.bind('ctrl+t', this.handleTranslationClick);
+  }
+
+  componentWillUnmount() {
+    Mousetrap.unbind('ctrl+t', this.handleTranslationClick);
+    if (this.translationWindow && !this.translationWindow.closed) {
+      this.translationWindow.close();
+    }
+  }
+
+  formatDocument() {
+    const fileObjects = this.props.document.files;
+    const fileNames = fileObjects ? fileObjects.map(metadata => metadata.path) : [];
+    const json = fileNames ? JSON.stringify(fileNames, null, 4) : null;
+    return json;
+  }
+
   render() {
-    const data = this.props.document.files;
-    const json = data ? JSON.stringify(data, null, 4) : null;
     return (
       <div className="Document col-md-9">
-        <pre>{json}</pre>
+        <pre>{this.formatDocument()}</pre>
+        <button onClick={this.handleTranslationClick} className="btn">T</button>
       </div>
     );
   }
